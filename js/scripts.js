136be09066731d3fbd06a2b5189bb1b7637eea16
Sam Kirsch and Shruti Priya <sam@>
2017-01-23 08:21:19 -0800

    create the basic html layout for the address book

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var Contact = function(first, last, street, city, state) {
+  this.firstName = first;
+  this.lastName = last;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
