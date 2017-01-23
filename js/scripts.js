8f47984d85b3f9e674e885e6583b143149dd181c
Sam Kirsch and Shruti Priya <shruti@>
2017-01-23 08:38:42 -0800

    add initial code of business logic to address-book

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,23 @@ var Contact = function(first, last, street, city, state) {
   this.city = city;
   this.state = state;
 }
+$(document).ready(function(){
+$("form#new-contact").submit(function(event){
+  event.preventDefault();
+ var inputtedFirstName = $("input#new-first-name").val();
+ var inputtedLastName = $("input#new-last-name").val();
+ var inputtedStreet = $("input#new-street").val();
+ var inputtedCity = $("input#new-city").val();
+ var inputtedState= $("input#new-state").val();
+
+ var newContact = new Contact(inputtedFirstName,inputtedLastName,inputtedStreet,inputtedCity,inputtedState);
+  $("ul#contacts").append("<li><span class ="contact">" + newContact.firstName + "</span></li>");
+
+  $("input#new-first-name").val();
+  $("input#new-last-name").val();
+  $("input#new-street").val();
+  $("input#new-city").val();
+  $("input#new-state").val();
+
+  });
+});
