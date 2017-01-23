49aaec5b4a288a304ac671fe2a44b79a508ccb1b
Sam Kirsch and Shruti Priya <shruti@>
2017-01-23 09:04:19 -0800

    add prototype to business logic and create user-interface logic for address-book

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business -logic goes here
+
 var Contact = function(first, last, street, city, state) {
   this.firstName = first;
   this.lastName = last;
@@ -5,17 +7,24 @@ var Contact = function(first, last, street, city, state) {
   this.city = city;
   this.state = state;
 }
+Contact.prototype.fullName =function(){
+  return this.firstName + " " + this.lastName;
+}
+
+
+// user-logic goes here
 $(document).ready(function(){
 $("form#new-contact").submit(function(event){
   event.preventDefault();
- var inputtedFirstName = $("input#new-first-name").val();
- var inputtedLastName = $("input#new-last-name").val();
- var inputtedStreet = $("input#new-street").val();
- var inputtedCity = $("input#new-city").val();
- var inputtedState= $("input#new-state").val();
+  var inputtedFirstName = $("input#new-first-name").val();
+  var inputtedLastName = $("input#new-last-name").val();
+  var inputtedStreet = $("input#new-street").val();
+  var inputtedCity = $("input#new-city").val();
+  var inputtedState= $("input#new-state").val();
 
- var newContact = new Contact(inputtedFirstName,inputtedLastName,inputtedStreet,inputtedCity,inputtedState);
-  $("ul#contacts").append("<li><span class ="contact">" + newContact.firstName + "</span></li>");
+  var newContact = new Contact(inputtedFirstName,inputtedLastName,inputtedStreet,inputtedCity,inputtedState);
+  console.log(newContact.street);
+  $("ul#contacts").append("<li><span class ='contact'>" + newContact.fullName() + "</span></li>");
 
   $("input#new-first-name").val();
   $("input#new-last-name").val();
@@ -23,5 +32,17 @@ $("form#new-contact").submit(function(event){
   $("input#new-city").val();
   $("input#new-state").val();
 
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".street").text(newContact.street);
+      $(".city").text(newContact.city);
+      $(".state").text(newContact.state);
+    });
   });
+
+
+
 });
